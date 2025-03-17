a40cd093ebd6f806c762fbc8155f443cfccbfd45
AREEBKHAN3306 <areebkhan3306@gmail.com>
2025-03-16 21:37:46 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Activity-4----Research-Compendium-Part-1
-Activity 4 -- Research Compendium Part 1
---
+++ my_script.R
@@ -0,0 +1,7 @@
+# Sample Code
+print("Hello, this is my research compendium activity in R!")
+
+x <- c(1, 2, 3, 4, 5)
+y <- x^2
+
+plot(x, y, type = "b", col = "blue", main = "Sample ")
